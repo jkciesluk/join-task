8e09f4ea110a3f177baf525cfa56c8cf4edac806
jkciesluk <323892@uwr.edu.pl>
2022-04-03 17:02:37 +0200

    Add K merge algorithm

--- main.scala
+++ main.scala
@@ -1,6 +1,10 @@
 import scala.io.Source
 import java.io.{File, BufferedWriter, FileWriter}
+import scala.collection.mutable.PriorityQueue
 // First I will sort both files using columin we are pertforming join on
+
+
+
 object Join {
   //returns iterator of chunks, with 1000 rows each
   def getIterator(file: String): Iterator[Array[String]] = 
@@ -39,6 +43,43 @@ object Join {
       lines: List[String] = sorted.map(_.mkString(",") + '\n')
     } yield  writeToTempFile(lines, file + n.toString)
     tempFiles
+  }
+
+  def mergeIterators(files: Iterator[File]): List[Iterator[Array[String]]] = 
+    files.map(Source.fromFile(_).getLines().map(_.split(','))).filter(_.nonEmpty).toList
+  
+  def kMerge(iterators: List[Iterator[Array[String]]], columnIndex: Int): List[Array[String]] = {
+    val ordering: Ordering[(Array[String], Iterator[Array[String]])] = Ordering.by(_._1(columnIndex))
+    KMerge.merge(KMerge.createQueue(iterators, ordering))
+  }
+}
+
+object KMerge {
+  def createQueue(iterators: List[Iterator[Array[String]]], 
+                  ordering: Ordering[(Array[String], Iterator[Array[String]])]
+                  ): PriorityQueue[(Array[String], Iterator[Array[String]])] = {
+
+    val heap: PriorityQueue[(Array[String], Iterator[Array[String]])] = PriorityQueue()(ordering)
+    val nonEmpty = iterators.filter(_.nonEmpty)
+    for {
+      iterator <- iterators
+      head = iterator.next()
+    } heap.addOne((head, iterator))
+    heap
+  }
+  def merge(queue: PriorityQueue[(Array[String], Iterator[Array[String]])]): List[Array[String]] = {
+    if(queue.nonEmpty) {
+      val (item, rest) = queue.dequeue()
+      if(rest.nonEmpty) {
+        item +: merge(queue.addOne(rest.next(), rest)) 
+      }
+      else {
+        item +: merge(queue)
+      }
+    }
+    else {
+      List.empty
+    }
   }
 }
 
